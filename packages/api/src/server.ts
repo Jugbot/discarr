4de4bd694af04d1fbabf4132aecdc520dda1b80a
Lucas Pollice <lp1935@nyu.edu>
2025-03-22 14:39:11 -0400

    reinstate cron job

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,29 +1,25 @@
 import { createHTTPServer } from '@trpc/server/adapters/standalone'
-import { appRouter } from './root'
-import { createTRPCContext } from './trpc'
 import cors from 'cors'
 import { createCaller } from '.'
+import { appRouter } from './root'
+import { createTRPCContext } from './trpc'
 
-// import * as cron from 'node-cron';
-// console.log('Running a task every minute');
-// cron.schedule('* * * * *', () => {
-//   const trpc = createCaller(createTRPCContext());
-//   console.log('Running a task every minute');
-//   // Call your tRPC procedures here
-//   // Example: await trpc.someProcedure();
-//   void trpc.post.hook({}).catch((error) => {
-//     console.error('Error running cron job:');
-//     console.error(error);
-//   })
-// });
+import * as cron from 'node-cron'
 
-const trpc = createCaller(createTRPCContext())
-// Call your tRPC procedures here
-// Example: await trpc.someProcedure();
-void trpc.post.hook().catch((error) => {
-  console.error('Error running cron job:')
-  console.error(error)
-})
+console.log('Running a task every minute')
+cron.schedule(
+  '* * * * *',
+  () => {
+    const trpc = createCaller(createTRPCContext())
+    void trpc.post.hook().catch((error) => {
+      console.error('Error running cron job:')
+      console.error(error)
+    })
+  },
+  {
+    runOnInit: true,
+  },
+)
 
 const server = createHTTPServer({
   router: appRouter,
