6b00549e0058b5094fbb492f318c2d4ad1effb98
Lucas Pollice <lpollice@liftoff.io>
2025-03-30 14:51:53 -0400

    Update post.ts

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -9,10 +9,11 @@ export const formatANSI = ansi.format
 export const postRouter = createTRPCRouter({
   ping: publicProcedure.query(() => 'pong'),
   hook: publicProcedure.mutation(async ({ ctx }) => {
-    logger.info('running media sync hook')
+    logger.info('Running media sync hook')
     const users = await getUsers()
     const medias = await getMedia(users)
 
+    logger.info(`Processing ${medias.length} items`)
     if (medias.length > 1000) {
       logger.warn(
         'More than 1000 media items to process, this is above the thread limit of discord. New threads will not be made until inactive threads are archived. Discord should do this automatically after 24 hours.',
@@ -22,9 +23,9 @@ export const postRouter = createTRPCRouter({
     // TODO: Re-enable parallel processing when contextual logging is added
     for (const media of medias) {
       await processMediaUpdate(ctx)(media)
-      logger.verbose(`done processing ${media.title}`)
+      logger.verbose(`Done processing ${media.title}`)
     }
 
-    logger.info('media sync hook complete')
+    logger.info('Media sync hook complete')
   }),
 })
