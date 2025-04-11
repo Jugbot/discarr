d765d28064e7de961986cf00836bd60009767fc4
Lucas Pollice <lp1935@nyu.edu>
2025-04-10 20:47:54 -0400

    Update schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -7,12 +7,12 @@ import {
   pgEnum,
 } from 'drizzle-orm/pg-core'
 
-const MediaType = pgEnum('media_type', ['movie', 'tv'])
+export const MediaType = pgEnum('media_type', ['movie', 'tv'])
 
 export const Media = pgTable(
   'media',
   {
-    jellyseerr_id: integer('jellyseerr_id').notNull().unique(),
+    jellyseerr_id: integer('jellyseerr_id').notNull(),
     type: MediaType('type').notNull(),
     thread_id: varchar('thread_id', { length: 255 }).notNull().unique(),
     // Last known state of media so we can generate faux-events
