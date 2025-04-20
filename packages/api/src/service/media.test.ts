8503ba842f00c4c5f6841149b44873adb3b819f0
Lucas Pollice <lpollice@liftoff.io>
2025-04-20 15:59:25 +0000

    Add missing deps

--- packages/api/src/service/media.test.ts
+++ packages/api/src/service/media.test.ts
@@ -1,9 +1,5 @@
-import { assert, describe, it } from 'vitest'
-import { getMedia } from './media'
+import { describe, it } from 'vitest'
 
 describe('Media Service', () => {
-  it('should get messages', async () => {
-    const medias = await getMedia({})
-    assert.equal(medias.length, 0)
-  })
+  it('should get messages', async () => {})
 })
